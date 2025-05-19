939235b9c3bb8f5522fd2358fc3a81d2b3bf594e
Pablo Tiscornia <pablotisco@gmail.com>
2025-05-19 15:26:36 -0600

    cargo clase 1

--- data/creo_datasets.R
+++ data/creo_datasets.R
@@ -22,17 +22,17 @@ base_original <- tibble(
 duplicados <- base_original %>% slice_sample(n = 50, replace = TRUE)
 
 # Unir original + duplicados y agregar algunos NAs
-datos_sucios <- bind_rows(base_original, duplicados) %>%
+df_ingresos_orig <- bind_rows(base_original, duplicados) %>%
   mutate(
     "Nombre Persona" = replace(`Nombre Persona`, sample(1:100, 5), NA),
     "Ingreso Anual $" = replace(`Ingreso Anual $`, sample(1:100, 8), NA)
   )
 
 # Cambiar aleatoriamente algunos valores a mayúsculas o minúsculas (para mostrar inconsistencias)
-datos_sucios <- change_case(datos_sucios, "Ciudad Residencia")
+df_ingresos_orig <- change_case(datos_sucios, "Ciudad Residencia")
 
 # Ver resultado
-datos_sucios
+df_ingresos_orig
 
 # Guardar como .csv
-write_csv(datos_sucios, "data/base_personas_ingreso_anual_2024.csv")
+write_csv(df_ingresos_orig, "data/base_personas_ingreso_anual_2024.csv")
